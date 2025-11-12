0fb609bc9eb2da4d8296d1530b03eeaf7156ec9b
Tursun <kurbanov.tursunchik@gmail.com>
2025-11-12 22:56:23 +0300

    second commit

--- src/deneme.java
+++ src/deneme.java
@@ -24,22 +24,52 @@ public class deneme {
 // }
 // System.out.println();
 
-int n =10;
-int i = 0;
-while (i <= n) {
-System.out.print("*");
-i++;
-}
-System.out.println();
+// int n =10;
+// int i = 0;
+// while (i <= n) {
+// System.out.print("*");
+// i++;
+// }
+// System.out.println();
 
-int par1 = 1, par2 = 129;
-while (par2 > par1)
-{
-if (par1 < 100) {
-System.out.println(par1);
-par1 *= 2;
-}
-} 
+// int par1 = 1, par2 = 129;
+// while (par2 > par1)
+// {
+// if (par1 < 100) {
+// System.out.println(par1);
+// par1 *= 2;
+// }
+
+// }
+// if ('A'>'a')
+// System.out.println("A>a");
+// else
+// System.out.println(“A<a");
+// if ('A'>'B')
+// System.out.println("A>B");
+// else
+// System.out.println("A<B");
+// if ('8'<'A')
+// System.out.println("8<A");
+// else
+// System.out.println("8>A");
+
+// double x = -32.2;
+// boolean isPositive = (x > 0);
+// if (isPositive = true)
+// System.out.println(x + " is positive");
+// else
+// System.out.println(x + " is not positive");
+
+int day = 3;
+String result = switch (day) {
+    case 1 -> "Monday";
+    case 2 -> "Tuesday";
+    case 3 -> "Wednesday";
+    default -> "Invalid day";
+};
+System.out.println(result);
+
     }
 }
 
