71398b53627fcc22137828401387c52f043b433d
Tursun <kurbanov.tursunchik@gmail.com>
2025-11-17 23:21:08 +0300

    editing

--- src/deneme.java
+++ src/deneme.java
@@ -164,20 +164,7 @@ public class deneme {
 //    System.out.println("Wrong answer");
 //   } 
 
-  int N = scanner.nextInt();
-  scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-if(N % 2 == 1){
-System.out.println("Weird");
-} else if (N % 2 == 0 && 2 <= N && N < 6){
-System.out.println("Not Weird");
-} else if (N % 2 == 0 && 6 <= N && N <= 20){
-System.out.println("Weird");
-}else if (N % 2 == 0 && 20 < N){
-System.out.println("Not Weird");
-}
-scanner.close();
-System.out.println("Hello, World.");
-        System.out.println("Hello, Java.");
+
 }
 
     }
