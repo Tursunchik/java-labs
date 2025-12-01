b1dab3ed1742dec7c9cb5e975e234f4ed43b3c56
Tursun <kurbanov.tursunchik@gmail.com>
2025-12-01 23:23:35 +0300

    trying arrays 2

--- src/deneme.java
+++ src/deneme.java
@@ -249,17 +249,28 @@ public class deneme {
 // for (int num : arr) {    
 //     System.out.println(num);
 
-int[] arr = {12, 5, 88, 44, 19};
+// int[] arr = {12, 5, 88, 44, 19};
         
-int max = arr[0];
+// int max = arr[0];
 
-for (int i = 1; i < arr.length; i++) {
-    if (arr[i] > max) {
-        max = arr[i];
-    }
-}
+// for (int i = 1; i < arr.length; i++) {
+//     if (arr[i] > max) {
+//         max = arr[i];
+//     }
+// }
 
-System.out.println("Maximum number: " + max);
+// System.out.println("Maximum number: " + max);
+
+int[] arr = {3, 7, 2, 9};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println("Sum = " + sum);
+        
 }
 }
 
