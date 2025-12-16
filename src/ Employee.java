e3e126dedcb25e6cf06a33a551af8a383386ea55
Tursun <kurbanov.tursunchik@gmail.com>
2025-12-16 11:56:51 +0300

    rework

--- src/ Employee.java
+++ src/ Employee.java
@@ -1,51 +1,74 @@
 import java.util.Scanner;
 
-public class  Employee {
+public class Employee {
+
     String department;
     String employeeID;
     String name;
     int yearsOfExperience;
     double baseSalary;
     boolean isFullTime;
+
     Scanner sc = new Scanner(System.in);
-    void readInformation(){
-       System.out.println("Employee ID: ");
-       this.employeeID = sc.nextLine();
 
-        System.out.println("Name: " );
-        this.name = sc.nextLine();
+    void readInformation() {
+        System.out.println("Employee ID:");
+        employeeID = sc.nextLine();
 
-        System.out.println("Years of study: " + yearsOfExperience);
-        this.yearsOfExperience = sc.nextInt();
+        System.out.println("Name:");
+        name = sc.nextLine();
 
-        System.out.println("GPA: " );
-        this.baseSalary = sc.nextDouble();
+        System.out.println("Years of experience:");
+        yearsOfExperience = sc.nextInt();
 
-        System.out.println("Full-Time (Yes/No): " + isFullTime);
-        this.isFullTime = sc.nextBoolean();
+        System.out.println("Base Salary:");
+        baseSalary = sc.nextDouble();
+        sc.nextLine(); 
+
+        System.out.println("Full-Time (Yes/No):");
+        String fullTimeInput = sc.nextLine();
+
+        isFullTime = fullTimeInput.equalsIgnoreCase("Yes");
     }
 
-    void predictAnnualIncome(int option){
-        int TurkeyIncome = 0;
-        int AbroadIncome = 0;
+    void predictAnnualIncome(int option) {
+        double income;
 
-        if(option == 1 && isFullTime == true){
-            TurkeyIncome += (baseSalary * 12) + (yearsOfExperience * 250) + 5000;
-        } else if( option == 1 && isFullTime == false){
-            TurkeyIncome += (baseSalary * 12) + (yearsOfExperience * 250) - 3000; 
-        } else if (option == 2 && isFullTime == true){
-            AbroadIncome += (baseSalary * 24) - (yearsOfExperience * 500) + 5000;
+        if (option == 1) { 
+            income = (baseSalary * 12) + (yearsOfExperience * 250);
+            if (isFullTime) {
+                income += 5000;
+            }
+            System.out.println(
+                "This employee earns " + income + " TL as annual income in Turkey."
+            );
         } else {
-            AbroadIncome += (baseSalary * 24) - (yearsOfExperience * 500) - 3000;
+            income = (baseSalary * 24) - (yearsOfExperience * 500);
+            if (!isFullTime) {
+                income -= 3000;
+            }
+            System.out.println(
+                "This employee earns " + income + " TL as annual income abroad."
+            );
         }
-        
-     }
-     void printInformation(){
-        if(isFullTime == true){
-            System.out.println(name + ", whose employee ID is  " + employeeID + ", is working in the " + department + "department as a full-time employee with " + yearsOfExperience + " years of experience. " + name + " has a base salary of " + baseSalary);
+    }
+
+    void printInformation() {
+        if (isFullTime) {
+            System.out.println(
+                name + ", whose employee ID is " + employeeID +
+                ", is working in the \"" + department + "\" department as a full-time employee with " +
+                yearsOfExperience + " years of experience. " +
+                name + " has a base salary of " + baseSalary + " TL."
+            );
         } else {
-            System.out.println(name + ", whose employee ID is  " + employeeID + ", is working in the " + department + "department as a part-time employee with " + yearsOfExperience + " years of experience. " + name + " has a base salary of " + baseSalary);
+            System.out.println(
+                name + ", whose employee ID is " + employeeID +
+                ", is working in the \"" + department + "\" department as a part-time employee with " +
+                yearsOfExperience + " years of experience. " +
+                name + " has a base salary of " + baseSalary + " TL."
+            );
         }
-        
-     }
+    }
 }
+
