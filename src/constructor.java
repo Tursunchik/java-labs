96e81d8a282ea7eead779af79304786320e5b87b
Tursun <kurbanov.tursunchik@gmail.com>
2026-02-15 17:44:31 +0300

    constructor

---
+++ src/constructor.java
@@ -0,0 +1,16 @@
+public class constructor {
+    public static void main(String[] args) {
+        
+    }
+
+ class   Student {
+    String name;
+    int age;
+
+
+    Student(String name , int age){
+        this.name = name;
+        this.age = age;
+    }
+    }
+}
